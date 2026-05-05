304e72d08bf5f5385cf926c395d5abd38e6f97bd
JxxHyk <choiieoe@naver.com>
2026-05-06 01:54:47 +0900

    feat: terraform 단일 EC2 인스턴스 환경 구축
    - 보안 그룹 수정 필요(인바운드 3000)

---
+++ terraform/data.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "amazon_linux_2023_arm64" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-kernel-6.1-arm64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
---
+++ terraform/main.tf
@@ -0,0 +1,105 @@
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "f1-vpc"
+  }
+}
+
+resource "aws_subnet" "this" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.0.1.0/24"
+
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-2a"
+  tags = {
+    Name = "f1-subnet-az-a"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "f1-igw"
+  }
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "f1-route-table"
+  }
+}
+
+resource "aws_route_table_association" "this" {
+  subnet_id      = aws_subnet.this.id
+  route_table_id = aws_route_table.this.id
+}
+
+resource "aws_security_group" "this" {
+  name   = "f1-sg"
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "f1-security-group"
+  }
+}
+
+resource "aws_security_group_rule" "ingress" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+resource "aws_security_group_rule" "ingress-ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = [var.my_ip]
+  security_group_id = aws_security_group.this.id
+}
+
+# 리버스 프록시 사용하기 전 임시 조치
+resource "aws_security_group_rule" "ingress-3000" {
+  type              = "ingress"
+  from_port         = 3000
+  to_port           = 3000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+resource "aws_instance" "this" {
+  ami             = data.aws_ami.amazon_linux_2023_arm64.id
+  instance_type   = "t4g.nano"
+  key_name        = "f1_board"
+  subnet_id       = aws_subnet.this.id
+  security_groups = [aws_security_group.this.id]
+
+  tags = {
+    Name = "f1_board"
+  }
+}

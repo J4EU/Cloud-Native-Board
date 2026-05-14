d1605ff21944d7b6e78c29e7195a3ebd0a8843fc
JxxHyk <choiieoe@naver.com>
2026-05-15 02:15:08 +0900

    style: fix variables description

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "my_house_ip" {
-  description = "My office public IP address for SSH access"
+  description = "My house public IP address for SSH access"
   type        = string
 }
 
 variable "my_office_ip" {
-  description = "My house public IP address for SSH access"
+  description = "My office public IP address for SSH access"
   type        = string
 }
